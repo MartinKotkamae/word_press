a9b1497543407cf87987669e04bd4ba5df097c9e
martinkotkamaeik <martin.kotkamae@khk.ee>
2021-02-22 11:44:43 +0200

    modified plugin `Very First Plugin` version 1.0 (after deactivation of `Very First Plugin` version 1.0)

--- wp-content/plugins/omaplugin/pluugiin.php
+++ wp-content/plugins/omaplugin/pluugiin.php
@@ -1,11 +1,27 @@
 <?php
 /**
  * Plugin Name: Very First Plugin
- * Plugin URl:
+ * Plugin URl: https://martinkotkamae.ikt.khk.ee/
  * Description: Esimene oma tehtud plugin.
  * Version: 1.0
  * Author: Martin Kotkamäe
  * Author URl: https://martinkotkamae.ikt.khk.ee/
 **/
 
+add_action("admin_menu", "addMenu");
+function addMenu()
+{
+    add_menu_page("Example Options", "Example Options", 4, "example-options", "exampleMenu");
+    add_submenu_page("example-options", "Option 1", "Option1", 4, "example-option-1", "option1");
+}
+
+function exampleMenu()
+{
+    echo "Hello World!";
+}
+
+function option1()
+{
+    echo "Valik!";
+}
 ?>
